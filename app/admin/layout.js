a648c0bef7e4358df520db3a4ada2f344abdea97
Chung <chung92726@gmail.com>
2023-07-08 00:52:03 +0800

    added check admin

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,47 +1,47 @@
-"use client";
+'use client'
 
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from 'react'
 
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import AdminNav from "../../components/nav/AdminNav";
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import AdminNav from '../../components/nav/AdminNav'
 
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify'
 
-const InstructorRoute = ({ children }) => {
-  const router = useRouter();
-  const [ok, setOk] = useState(true);
+const InstructorRoute = ({ children, params }) => {
+  const router = useRouter()
+  const [ok, setOk] = useState(false)
 
-  // useEffect(() => {
-  //   const fetchInstructor = async () => {
-  //     try {
-  //       const { data } = await axios.get('/api/current-instructor')
-  //       console.log(data)
-  //       if (data.ok) setOk(true)
-  //     } catch (err) {
-  //       console.log(err)
-  //       toast.error('You are not authorized')
-  //       router.push('/')
-  //     }
-  //   }
-  //   fetchInstructor()
-  // }, [])
+  const fetchAdmin = async () => {
+    try {
+      const { data } = await axios.get('/api/is-admin')
+      console.log(data)
+      if (data.ok) setOk(true)
+    } catch (err) {
+      console.log(err)
+      toast.error('You are not authorized')
+      router.push('/')
+    }
+  }
+  useEffect(() => {
+    fetchAdmin()
+  }, [params])
   return (
     <>
       {ok && (
-        <div className="flex flex-row w-full bg-gray-100">
-          <div className="nav_container">
+        <div className='flex flex-row w-full bg-gray-100'>
+          <div className='nav_container'>
             <AdminNav />
           </div>
-          <div className="w-full flex justify-end h-[93vh] ">
-            <div className="w-full pl-[13rem] flex justify-center items-start ">
+          <div className='w-full flex justify-end h-[93vh] '>
+            <div className='w-full pl-[13rem] flex justify-center items-start '>
               {children}
             </div>
           </div>
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default InstructorRoute;
+export default InstructorRoute
