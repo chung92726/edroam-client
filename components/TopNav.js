9450ae8993f52503e1a69e0d797b15ab2680aa86
mac <>
2023-07-11 13:24:57 +0800

    Updated TopNav MyLearning display for no courses enrolled users.

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,23 +1,22 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useState, useEffect, useContext } from "react";
-import { AiOutlineLogin } from "react-icons/ai";
-import { RiRegisteredLine } from "react-icons/ri";
-import { ToastContainer, toast } from "react-toastify";
-import { usePathname } from "next/navigation";
-import "react-toastify/dist/ReactToastify.css";
-import { Context } from "../context/index";
-import { useRouter } from "next/navigation";
-import { FaChalkboardTeacher } from "react-icons/fa";
-import { BsBook, BsShop } from "react-icons/bs";
-import { IoCreate } from "react-icons/io5";
-import axios from "axios";
-import MyLearningMenu from "./MyLearningMenu";
-
+import Link from 'next/link';
+import { useState, useEffect, useContext } from 'react';
+import { AiOutlineLogin } from 'react-icons/ai';
+import { RiRegisteredLine } from 'react-icons/ri';
+import { ToastContainer, toast } from 'react-toastify';
+import { usePathname } from 'next/navigation';
+import 'react-toastify/dist/ReactToastify.css';
+import { Context } from '../context/index';
+import { useRouter } from 'next/navigation';
+import { FaChalkboardTeacher } from 'react-icons/fa';
+import { BsBook, BsShop } from 'react-icons/bs';
+import { IoCreate } from 'react-icons/io5';
+import axios from 'axios';
+import MyLearningMenu from './MyLearningMenu';
 
 const TopNav = () => {
-  const [currentPage, setCurrentPage] = useState("");
+  const [currentPage, setCurrentPage] = useState('');
   const path = usePathname();
 
   //global state
@@ -30,11 +29,11 @@ const TopNav = () => {
 
   const logout = async () => {
     try {
-      const { data } = await axios.get("/api/logout");
-      dispatch({ type: "LOGOUT" });
-      window.localStorage.removeItem("user");
-      toast.success("Logout Successfully");
-      router.push("/login");
+      const { data } = await axios.get('/api/logout');
+      dispatch({ type: 'LOGOUT' });
+      window.localStorage.removeItem('user');
+      toast.success('Logout Successfully');
+      router.push('/login');
     } catch (err) {
       toast.error(err.response.data);
     }
@@ -49,83 +48,83 @@ const TopNav = () => {
   };
 
   return (
-    <div className="flex flex-col w-full fixed z-50">
-      <div className="navbar bg-base-100 h-[70px]">
-        <ToastContainer position="top-center" />
-        <div className="flex-1 max-md:justify-left">
+    <div className='flex flex-col w-full fixed z-50'>
+      <div className='navbar bg-base-100 h-[70px]'>
+        <ToastContainer position='top-center' />
+        <div className='flex-1 max-md:justify-left'>
           <Link
-            href="/"
-            className="btn btn-ghost normal-case text-xl"
-            onClick={() => setCurrentPage("home")}
+            href='/'
+            className='btn btn-ghost normal-case text-xl'
+            onClick={() => setCurrentPage('home')}
           >
-            <img src="/Proedu.png" className="w-[100px]" />
+            <img src='/Proedu.png' className='w-[100px]' />
           </Link>
           {user && user.role ? (
-            user.role.includes("Instructor") ? (
+            user.role.includes('Instructor') ? (
               <Link
-                href="/instructor/course/create"
-                className="mx-4 my-1 cursor-pointer "
-                onClick={() => setCurrentPage("login")}
+                href='/instructor/course/create'
+                className='mx-4 my-1 cursor-pointer max-md:hidden'
+                onClick={() => setCurrentPage('login')}
               >
-                <div className="hidden md:flex flex-row items-center">
-                  <div className="btn btn-ghost rounded-btn text-[12px] max-md:hidden">
-                    <IoCreate className="inline-block " />
-                    <p className="ml-[-5px]">Create Course</p>
+                <div className='hidden md:flex flex-row items-center'>
+                  <div className='btn btn-ghost rounded-btn text-[12px] max-md:hidden'>
+                    <IoCreate className='inline-block ' />
+                    <p className='ml-[-5px]'>Create Course</p>
                   </div>
                 </div>
               </Link>
             ) : (
               <Link
-                href="/user/become-instructor"
-                className="mx-4 my-1 cursor-pointer max-md:hidden"
-                onClick={() => setCurrentPage("login")}
+                href='/user/become-instructor'
+                className='mx-4 my-1 cursor-pointer max-md:hidden'
+                onClick={() => setCurrentPage('login')}
               >
-                <div className="hidden md:flex flex-row items-center">
-                  <div className="btn btn-ghost rounded-btn text-[12px]">
-                    <FaChalkboardTeacher className="inline-block ml-[-5px]" />
-                    <p className=" ">Become Instructor</p>
+                <div className='hidden md:flex flex-row items-center'>
+                  <div className='btn btn-ghost rounded-btn text-[12px]'>
+                    <FaChalkboardTeacher className='inline-block ml-[-5px]' />
+                    <p className=' '>Become Instructor</p>
                   </div>
                 </div>
               </Link>
             )
           ) : null}
 
-          <div className="dropdown dropdown-end">
+          <div className='dropdown dropdown-end'>
             <label
               tabIndex={0}
-              className="btn btn-ghost rounded-btn max-sm:!pr-0"
+              className='btn btn-ghost rounded-btn px-0 max-md:!pl-4'
             >
-              <div className="flex flex-row items-center text-[12px]">
-                <BsShop className="inline-block mx-[0.5px]" />
-                <p className="mx-1">Browse Course</p>
+              <div className='flex flex-row items-center text-[12px]'>
+                <BsShop className='inline-block mx-[0.5px]' />
+                <p className='mx-1'>Browse Course</p>
               </div>
             </label>
             <ul
               tabIndex={0}
-              className="menu dropdown-content p-2 shadow bg-base-200 rounded-box w-64 mt-4"
+              className='menu dropdown-content p-2 shadow bg-base-200 rounded-box w-64 mt-4'
             >
               <li>
-                <Link href="/marketplace">All Courses</Link>
+                <Link href='/marketplace'>All Courses</Link>
               </li>
               <li>
-                <Link href="/marketplace/WebDesign">WebDesign</Link>
+                <Link href='/marketplace/WebDesign'>WebDesign</Link>
                 {/* <a>Web Development Courses</a> */}
               </li>
               <li>
-                <Link href="/marketplace/UIUXDesign">UI/UX Design Courses</Link>
+                <Link href='/marketplace/UIUXDesign'>UI/UX Design Courses</Link>
                 {/* <a>UI/UX Design Courses</a> */}
               </li>
               <li>
-                <Link href="/marketplace/GraphicDesign">
+                <Link href='/marketplace/GraphicDesign'>
                   Graphic Design Courses
                 </Link>
               </li>
               <li>
-                <Link href="/marketplace/3DModeling">3D Modelling Courses</Link>
+                <Link href='/marketplace/3DModeling'>3D Modelling Courses</Link>
                 {/* <a>3D Modelling Courses</a> */}
               </li>
               <li>
-                <Link href="/marketplace/VideoEditing">
+                <Link href='/marketplace/VideoEditing'>
                   Video Editing Courses
                 </Link>
                 {/* <a>Video Editing Courses</a> */}
@@ -134,22 +133,20 @@ const TopNav = () => {
           </div>
         </div>
         {user ? (
-          <div className="hidden md:flex dropdown dropdown-end">
+          <div className='hidden md:flex dropdown dropdown-end'>
             <label
               tabIndex={0}
-              className="btn btn-ghost rounded-btn max-sm:!pr-0"
+              className='btn btn-ghost rounded-btn max-sm:!pr-0'
             >
-              <div className="flex flex-row items-center text-[12px] gap-2 mr-2">
-                <BsBook className="inline-block mx-[0.5px]" />
+              <div className='flex flex-row items-center text-[12px] gap-2 mr-2'>
+                <BsBook className='inline-block mx-[0.5px]' />
                 <p>My Learning</p>
               </div>
             </label>
-            <MyLearningMenu
-              tabIndex={0}
-            />
+            <MyLearningMenu tabIndex={0} />
           </div>
         ) : null}
-        <div className="hidden md:flex flex-row gap-2 mr-10">
+        <div className='hidden md:flex flex-row gap-2 mr-10'>
           {/* <div className='form-control'>
           <input
             type='text'
@@ -158,22 +155,22 @@ const TopNav = () => {
           />
           </div> */}
           {user ? (
-            <div className="dropdown dropdown-end m-2 font-sans">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-8 rounded-full">
-                  <img src={"/guest.png"} />
+            <div className='dropdown dropdown-end m-2 font-sans'>
+              <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
+                <div className='w-8 rounded-full'>
+                  <img src={'/guest.png'} />
                 </div>
               </label>
               <ul
                 tabIndex={0}
-                className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-gray-100 rounded-box w-52"
+                className='mt-3 p-2 shadow menu menu-sm dropdown-content bg-gray-100 rounded-box w-52'
               >
                 <li>
-                  <Link href="/user/profile">Profile</Link>
+                  <Link href='/user/profile'>Profile</Link>
                 </li>
-                {user && user.role && user.role.includes("Instructor") && (
+                {user && user.role && user.role.includes('Instructor') && (
                   <li>
-                    <Link href="/instructor">Instructor Dashboard</Link>
+                    <Link href='/instructor'>Instructor Dashboard</Link>
                   </li>
                 )}
                 <li>
@@ -185,32 +182,32 @@ const TopNav = () => {
               </ul>
             </div>
           ) : (
-            <ul className="menu menu-horizontal">
+            <ul className='menu menu-horizontal'>
               <Link
-                href="/login"
-                className="mx-2 my-1 cursor-pointer border-transparent"
-                onClick={() => setCurrentPage("login")}
+                href='/login'
+                className='mx-2 my-1 cursor-pointer border-transparent'
+                onClick={() => setCurrentPage('login')}
               >
-                <div className="flex flex-row items-center">
-                  <AiOutlineLogin className="inline-block mx-[0.5px]" />
-                  <p className="mx-1">Login</p>
+                <div className='flex flex-row items-center'>
+                  <AiOutlineLogin className='inline-block mx-[0.5px]' />
+                  <p className='mx-1'>Login</p>
                 </div>
               </Link>
 
               <Link
-                href="/register"
-                className="mx-2 my-1 cursor-pointer border-transparent"
-                onClick={() => setCurrentPage("register")}
+                href='/register'
+                className='mx-2 my-1 cursor-pointer border-transparent'
+                onClick={() => setCurrentPage('register')}
               >
-                <div className="flex flex-row items-center">
-                  <RiRegisteredLine className="inline-block mx-[0.5px]" />
-                  <p className="mx-1">Sign up</p>
+                <div className='flex flex-row items-center'>
+                  <RiRegisteredLine className='inline-block mx-[0.5px]' />
+                  <p className='mx-1'>Sign up</p>
                 </div>
               </Link>
             </ul>
           )}
         </div>
-        <div className="md:hidden flex flex-row gap-2 pr-5 max-sm:pr-0">
+        <div className='md:hidden flex flex-row gap-2 pr-5 max-sm:pr-0'>
           {/* <div className='form-control'>
           <input
             type='text'
@@ -219,36 +216,36 @@ const TopNav = () => {
           />
           </div> */}
           {user ? (
-            <div className="dropdown dropdown-end m-2 font-sans">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-8 mr-2 rounded-full">
-                  <img src={"/guest.png"} />
+            <div className='dropdown dropdown-end m-2 font-sans'>
+              <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
+                <div className='w-8 mr-2 rounded-full'>
+                  <img src={'/guest.png'} />
                 </div>
               </label>
               <ul
                 tabIndex={0}
-                className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-gray-100 rounded-box w-44"
+                className='mt-3 p-2 shadow menu menu-sm dropdown-content bg-gray-100 rounded-box w-44'
               >
                 <li>
-                  <Link href="/user/profile">Profile</Link>
+                  <Link href='/user/profile'>Profile</Link>
                 </li>
-                {user && user.role && user.role.includes("Instructor") ? (
+                {user && user.role && user.role.includes('Instructor') ? (
                   <li>
-                    <Link href="/instructor">Instructor Dashboard</Link>
-                    <Link href="/instructor/course/create">Create Course</Link>
+                    <Link href='/instructor'>Instructor Dashboard</Link>
+                    <Link href='/instructor/course/create'>Create Course</Link>
                   </li>
                 ) : (
                   <li>
                     <Link
-                      href="/user/become-instructor"
-                      onClick={() => setCurrentPage("login")}
+                      href='/user/become-instructor'
+                      onClick={() => setCurrentPage('login')}
                     >
                       Become Instructor
                     </Link>
                   </li>
                 )}
                 <li>
-                  <Link href="/user">My Learning</Link>
+                  <Link href='/user'>My Learning</Link>
                 </li>
                 <li>
                   <a>Settings</a>
@@ -259,38 +256,38 @@ const TopNav = () => {
               </ul>
             </div>
           ) : (
-            <div className="dropdown dropdown-end m-2 font-sans">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-8 mr-2 rounded-full">
-                  <img src={"/guest.png"} />
+            <div className='dropdown dropdown-end m-2 font-sans'>
+              <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
+                <div className='w-8 mr-2 rounded-full'>
+                  <img src={'/guest.png'} />
                 </div>
               </label>
               <ul
                 tabIndex={0}
-                className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-gray-100 rounded-box w-52 z-40"
+                className='mt-3 p-2 shadow menu menu-sm dropdown-content bg-gray-100 rounded-box w-52 z-40'
               >
                 <li>
                   <Link
-                    href="/login"
-                    className="mx-2 my-1 cursor-pointer border-transparent"
-                    onClick={() => setCurrentPage("login")}
+                    href='/login'
+                    className='mx-2 my-1 cursor-pointer border-transparent'
+                    onClick={() => setCurrentPage('login')}
                   >
-                    <div className="flex flex-row items-center">
-                      <AiOutlineLogin className="inline-block mx-[0.5px]" />
-                      <p className="mx-1">Login</p>
+                    <div className='flex flex-row items-center'>
+                      <AiOutlineLogin className='inline-block mx-[0.5px]' />
+                      <p className='mx-1'>Login</p>
                     </div>
                   </Link>
                 </li>
 
                 <li>
                   <Link
-                    href="/register"
-                    className="mx-2 my-1 cursor-pointer border-transparent"
-                    onClick={() => setCurrentPage("register")}
+                    href='/register'
+                    className='mx-2 my-1 cursor-pointer border-transparent'
+                    onClick={() => setCurrentPage('register')}
                   >
-                    <div className="flex flex-row items-center">
-                      <RiRegisteredLine className="inline-block mx-[0.5px]" />
-                      <p className="mx-1">Sign up</p>
+                    <div className='flex flex-row items-center'>
+                      <RiRegisteredLine className='inline-block mx-[0.5px]' />
+                      <p className='mx-1'>Sign up</p>
                     </div>
                   </Link>
                 </li>
@@ -299,7 +296,7 @@ const TopNav = () => {
           )}
         </div>
       </div>
-      <div className="text-center bg-gradient-to-r from-sky-500 to-indigo-500 text-yellow-100 w-full rounded h-[4px] flex flex-col justify-center text-[28px] items-start font-bold "></div>
+      <div className='text-center bg-gradient-to-r from-sky-500 to-indigo-500 text-yellow-100 w-full rounded h-[4px] flex flex-col justify-center text-[28px] items-start font-bold '></div>
     </div>
   );
 };
--- components/cards/LearningCard.js
+++ components/cards/LearningCard.js
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import axios from 'axios';
 
 const LearningCard = () => {
   const [myLearning, setMyLearning] = useState([]);
@@ -16,7 +16,7 @@ const LearningCard = () => {
   };
 
   const loadCourses = async () => {
-    const { data } = await axios.get("/api/user-courses");
+    const { data } = await axios.get('/api/user-courses');
 
     for (let i = 0; i < data.length; i++) {
       // console.log(data[i].lessons.length);
@@ -33,44 +33,67 @@ const LearningCard = () => {
   }, []);
 
   return (
-    <div className="flex flex-col justify-center items-center w-full divide-y-2 divide-black-500">
-      {myLearning.map((course) => (
-        <Link
-          className="flex flex-row w-full  cursor-pointer group p-3 "
-          href={`/user/course/${course.slug}`}
-          key={course._id}
-        >
-          <figure className="mr-2 w-2/6">
-            <img
-              src={course.image ? course.image.Location : "/course.png"}
-              alt="course image"
-              className="rounded w-[80px] h-[80px]  object-cover filter  "
-            />
-          </figure>
-          <div className="flex flex-col justify-end w-4/6">
-            <div className="">
-              <p className="text-left font-semibold	 break-words"> {course.name.length > 100 ? `${course.name.substring(0, 100)}...` : course.name}</p>
+    <div className='flex flex-col justify-center items-center w-full divide-y-2 divide-black-500'>
+      {myLearning.length !== 0 ? (
+        myLearning.map((course) => (
+          <Link
+            className='flex flex-row w-full  cursor-pointer group p-3 '
+            href={`/user/course/${course.slug}`}
+            key={course._id}
+          >
+            <figure className='mr-2 w-2/6'>
+              <img
+                src={course.image ? course.image.Location : '/course.png'}
+                alt='course image'
+                className='rounded w-[80px] h-[80px]  object-cover filter  '
+              />
+            </figure>
+            <div className='flex flex-col justify-end w-4/6'>
+              <div className=''>
+                <p className='text-left font-semibold	 break-words'>
+                  {' '}
+                  {course.name.length > 100
+                    ? `${course.name.substring(0, 100)}...`
+                    : course.name}
+                </p>
+              </div>
+              <div className='w-full'>
+                <progress
+                  className='progress w-full'
+                  value={course.completed}
+                  max={course.lessons.length}
+                >
+                  <span></span>
+                </progress>
+              </div>
             </div>
-            <div className="w-full">
-              <progress
-                className="progress w-full"
-                value={course.completed}
-                max={course.lessons.length}
-              >
-                <span></span>
-              </progress>
-            </div>
-          </div>
-        </Link>
-      ))}
-      <div className="w-full justify-center p-3 border-t-2 border-black-500">
-              <Link 
-              href="/user">
-                <button className="btn btn-primary bg-indigo-500 w-full">
-                  Go to My Learning
-                </button>
-              </Link>
-            </div>
+          </Link>
+        ))
+      ) : (
+        <div className='flex flex-col p-8 justify-center items-center'>
+          <h3 className='text-lg font-sans font-semibold py-4'>
+            No course enrolled yet
+          </h3>
+          <p className='text-center font-sans text-slate-500 pb-4'>
+            Let's go to marketplace to explore more
+          </p>
+        </div>
+      )}
+      <div className='w-full justify-center p-3 border-t-2 border-black-500'>
+        {myLearning.length !== 0 ? (
+          <Link href='/user'>
+            <button className='btn btn-primary bg-indigo-500 w-full'>
+              Go to My Learning
+            </button>
+          </Link>
+        ) : (
+          <Link href='/marketplace'>
+            <button className='btn btn-primary bg-indigo-500 w-full'>
+              Start Learning Today!
+            </button>
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
