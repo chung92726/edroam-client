081af8dd1ad82bb38e60af956d5674c55c338f66
Chung <chung92726@gmail.com>
2023-08-08 18:42:27 +0800

    delete config production condition

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   async rewrites() {
-    if (process.env.NODE_ENV !== 'production') {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://127.0.0.1:8000/api/:path*', // Proxy to Backend
-        },
-      ]
-    }
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/api/:path*', // Proxy to Backend
+      },
+    ]
   },
 }
