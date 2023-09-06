1be712fdd2b1c9d3bfd0ee472f295925064f181b
Chung <chung92726@gmail.com>
2023-09-06 14:29:18 +0800

    modified the login and register page

--- app/[lang]/register/register.js
+++ app/[lang]/register/register.js
@@ -1,52 +1,54 @@
-'use client';
+'use client'
 
-import { useState, useEffect, useContext } from 'react';
-import { Context } from '@/context/index';
-import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import { useState, useEffect, useContext } from 'react'
+import { Context } from '@/context/index'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { FacebookLoginButton } from 'react-social-login-buttons'
+import { GoogleLoginButton } from 'react-social-login-buttons'
 
 const Register = ({ register }) => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [promotion, setPromotion] = useState(true);
-  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [promotion, setPromotion] = useState(true)
+  const [loading, setLoading] = useState(false)
   //   const [confirmPassword, setConfirmPassword] = useState('')
 
-  const router = useRouter();
+  const router = useRouter()
 
   // global state
-  const { state, dispatch } = useContext(Context);
-  const { user } = state;
+  const { state, dispatch } = useContext(Context)
+  const { user } = state
 
   useEffect(() => {
-    if (user !== null) router.push('/user');
-  }, [user]);
+    if (user !== null) router.push('/user')
+  }, [user])
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     // console.table({ name, email, password })
-    setLoading(true);
+    setLoading(true)
     try {
       const { data } = await axios.post(`/api/register`, {
         name,
         email,
         password,
         promotion,
-      });
-      toast.success(`${register.RegSuccess}`);
-      setName('');
-      setEmail('');
-      setPassword('');
-      setLoading(false);
-      router.push('/login');
+      })
+      toast.success(`${register.RegSuccess}`)
+      setName('')
+      setEmail('')
+      setPassword('')
+      setLoading(false)
+      router.push('/login')
     } catch (err) {
-      toast.error(err.response.data);
-      setLoading(false);
+      toast.error(err.response.data)
+      setLoading(false)
     }
-  };
+  }
   return (
     <>
       {/* <ToastContainer position='top-center' /> */}
@@ -56,8 +58,50 @@ const Register = ({ register }) => {
           onSubmit={handleSubmit}
           className='justify-center items-center form-control max-sm:mx-5'
         >
-          <div className='w-full max-w-sm my-2 flex'>
-            <h2 className='text-base font-bold mx-2'>{register.Title}</h2>
+          <div className='w-full max-w-sm my-2 flex flex-col'>
+            <h2 className='text-[20px] font-bold mx-2 my-2 '>
+              {register.Title}
+            </h2>
+            <p className='mx-2 text-gray-400'>{register.Subtitle}</p>
+          </div>
+          <div className='grid grid-cols-2 gap-6 w-full max-w-sm'>
+            <a href='/api/auth/facebook' className='w-full max-w-sm'>
+              <FacebookLoginButton
+                className='w-full my-3 font-bold'
+                placeholder='Facebook'
+                style={{
+                  fontSize: '14px',
+
+                  borderRadius: '5px',
+                  boxShadow: 'rgba(0, 0, 0, 0.8) 2px 2px 5px',
+                }}
+              >
+                <span>Facebook</span>
+              </FacebookLoginButton>
+            </a>
+            <a href='/api/auth/google' className='w-full max-w-sm'>
+              <GoogleLoginButton
+                className='w-12  my-3 font-bold'
+                style={{
+                  fontSize: '14px',
+                  borderRadius: '5px',
+                  boxShadow: 'rgba(0, 0, 0, 0.8) 2px 2px 5px',
+                }}
+              >
+                <span>Google</span>
+              </GoogleLoginButton>
+            </a>
+          </div>
+
+          <div className='relative w-full max-w-sm my-3'>
+            <div className='absolute inset-0 flex items-center'>
+              <span className='w-full border-t-2 border-base-300' />
+            </div>
+            <div className='relative flex justify-center text-xs uppercase'>
+              <span className='bg-background px-2 text-muted-foreground'>
+                Or continue with
+              </span>
+            </div>
           </div>
           <input
             type='text'
@@ -128,7 +172,7 @@ const Register = ({ register }) => {
         </form>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Register;
+export default Register
